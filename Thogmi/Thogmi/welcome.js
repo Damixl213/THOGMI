1455c95fd94d4f8bfc23f533015d97b73588a4e0
David Oyeyipo <159963433+Damixl213@users.noreply.github.com>
2024-12-26 09:39:13 +0100

    Delete Thogmi/Thogmi directory

--- Thogmi/Thogmi/welcome.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    alert("Authourizion in process?!");
-    // You can add your login logic here
-  }
-
-  function signup() {
-    alert("Sign Up button clicked!");
-    // You can add your sign up logic here
-  }
